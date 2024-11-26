c8e6b21bc5667dea682d5bcf2c10d370b1fb4d23
Daniela Lomas <daniela.lomaso@uanl.edu.mx>
2024-11-25 22:01:16 -0600

    Pantallas de Dueño
    Todas las pantallas de dueño, P14=PANTALLA PRINCIPAL y P15=ADMINISTRACIÓN DE PERSONAL

---
+++ BEARFLIX/Views/Pantallas de Dueño/P14/Script-P14.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Botón Usuarios de la Plataforma
+    $("#users-btn").click(function () {
+      window.location.href = "usuarios.html"; // Redirige a usuarios.html
+    });
+  
+    // Botón Reporte de Ventas
+    $("#sales-btn").click(function () {
+      window.location.href = "reporte-ventas.html"; // Redirige a reporte-ventas.html
+    });
+  
+    // Botón Administración de Personal
+    $("#staff-btn").click(function () {
+      window.location.href = "admin-personal.html"; // Redirige a admin-personal.html
+    });
+  });
+  
